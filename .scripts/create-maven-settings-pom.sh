6423bc0068504d15ba20d28d24003f8b7af75898
Bernardo <programming@wandrell.com>
2017-04-22 12:38:00 +0200

    Added new parameters to the doc comments

--- .scripts/create-maven-settings-pom.sh
+++ .scripts/create-maven-settings-pom.sh
@@ -36,6 +36,8 @@
 #
 # The function expects the following parameters:
 # $1: A string, one of release|develop, otherwise it is ignored.
+# $2: A string, the name of the profile with the release deployment configuration.
+# $3: A string, the name of the profile with the snapshot deployment configuration.
 #
 # --- ENVIRONMENTAL VARIABLES ---
 #
